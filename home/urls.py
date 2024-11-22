6bfc5c569f51ca0312b1bb2c49004d4476b36df6
Islânio Pablo <islaniopbl@gmail.com>
2024-11-22 14:35:19 -0300

    .

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('contato', views.contato, name='contato'),
     path('ajuda', views.ajuda, name='ajuda'),
     path('item/<int:id>/', views.exibir_item, name='exibir_item'),
-    path('perfil/<str:usuario>/', views.perfil, name='perfil')
+    path('perfil/<str:usuario>/', views.perfil, name='perfil'),
+    path('dia/<int:numero>/', views.dia_da_semana, name='dia_da_semana'),
 ]
--- home/views.py
+++ home/views.py
@@ -16,3 +16,34 @@ def ajuda(request):
 
 def exibir_item(request, id):
     return render(request, "exibir_item.html",{'id':id})
+
+def perfil(request, usuario):
+   
+    context = {
+        'usuario': usuario
+    }
+   
+    return render(request, 'perfil.html', context)
+
+from django.shortcuts import render
+
+from django.shortcuts import render
+
+def dia_da_semana(request, numero):
+    
+    dias_da_semana = [
+        "Erro! Insira um valor entre 1 e 7.",  # Posição 0 para erro
+        "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", 
+        "Quinta-feira", "Sexta-feira", "Sábado"
+    ]
+
+  
+    if 1 <= numero <= 7:
+        dia = dias_da_semana[numero]
+    else:
+        dia = dias_da_semana[0]  
+
+    
+    context = {'dia': dia}
+    return render(request, 'dia_da_semana.html', context)
+
